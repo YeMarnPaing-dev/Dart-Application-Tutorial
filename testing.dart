a8625bf026c3458bcc2a55f9cd12a48c3b2ff0d9
Saw Ye Marn <yemarnpaing44@gmail.com>
2025-09-22 03:29:26 +0630

    type

--- testing.dart
+++ testing.dart
@@ -1,12 +1,20 @@
 void main(){
-  print(fatherName);
+
+
+
+// String fatherName = 'U Ba';
+// int intgerNumber = 1234;
+// double decimalNumber = 11;
+// num age = 1.12;
+// bool condition = true;
+// List nameList = ['Mg Mg','Mya'];
+// List ageList = [12,33,4];
+// Map dictionart = {'name':'Mg','age':'20'};
+
+String greeting = 'helloworld';
+print(greeting.toUpperCase());
+
+
+
 
 }
-String fatherName = 'U Ba';
-int intgerNumber = 1234;
-double decimalNumber = 11;
-num age = 1.12;
-bool condition = true;
-List nameList = ['Mg Mg','Mya'];
-List ageList = [12,33,4];
-Map dictionart = {'name':'Mg','age':'20'};
