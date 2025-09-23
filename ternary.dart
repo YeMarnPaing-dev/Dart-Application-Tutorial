05a2b2003ca074b3c3d1c77fa7ca089d4de24a44
Saw Ye Marn <yemarnpaing44@gmail.com>
2025-09-23 10:02:51 +0630

    taranary

---
+++ ternary.dart
@@ -0,0 +1,14 @@
+void main(){
+  String ?  message ;
+  
+  const score = 83;
+  
+  if(score > 60){
+    message='You passed';
+      print(message);
+  }else{
+    message='You Fail';
+      print(message);
+  }
+
+}
