4b8ad9c6772281ecd5a9e954e5dab92826eabe01
Saw Ye Marn <yemarnpaing44@gmail.com>
2025-09-23 10:33:41 +0630

    while

---
+++ why_doWhile.dart
@@ -0,0 +1,16 @@
+void main(){
+   int moneyBox = 0;
+   while(moneyBox < 1000){
+    moneyBox = moneyBox + 100;
+    print(moneyBox);
+   }
+   print('While Loop is complete');
+
+   int num1 = 10;
+   do{
+    num1 += 5;
+    print(num1);
+   }while(num1<20);
+    print('exit Do while loop');
+   
+}
